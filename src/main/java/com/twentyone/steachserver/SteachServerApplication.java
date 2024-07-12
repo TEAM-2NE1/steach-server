7d708b8e0033ce659c35f62f08b8f3aa4cc12baa
Juhyorim <joohr1234@gmail.com>
2024-07-12 17:37:30 +0900

    refactor: 초기세팅 변경

---
+++ src/main/java/com/twentyone/steachserver/SteachServerApplication.java
@@ -0,0 +1,13 @@
+package com.twentyone.steachserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteachServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteachServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/twentyone/steachserver/SteachServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twentyone.steachserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteachServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
