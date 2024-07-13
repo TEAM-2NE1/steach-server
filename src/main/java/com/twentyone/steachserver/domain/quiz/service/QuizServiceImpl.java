53f0bb2154178b1dc9d76b29ac5930c701cdb9db
sihyunjojo <si4018@naver.com>
2024-07-13 18:13:25 +0900

    feat: Quiz 관련 클래스 추가 및 퀴즈 생성 기능 완료
    QuizChoice 엔터티 및 서비스 구현, createQuizChoice 메서드 및 QuizChoiceRepository 추가
    퀴즈 생성 기능 완료

---
+++ src/main/java/com/twentyone/steachserver/domain/quiz/service/QuizServiceImpl.java
@@ -0,0 +1,47 @@
+package com.twentyone.steachserver.domain.quiz.service;
+
+import com.twentyone.steachserver.domain.lecture.Lecture;
+import com.twentyone.steachserver.domain.lecture.service.LectureService;
+import com.twentyone.steachserver.domain.quiz.dto.QuizRequestDto;
+import com.twentyone.steachserver.domain.quiz.dto.QuizResponseDto;
+import com.twentyone.steachserver.domain.quiz.model.Quiz;
+import com.twentyone.steachserver.domain.quiz.model.QuizChoice;
+import com.twentyone.steachserver.domain.quiz.repository.QuizRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+@RequiredArgsConstructor
+public class QuizServiceImpl implements QuizService {
+
+    private QuizRepository quizRepository;
+
+    private QuizChoiceService quizChoiceService;
+    private LectureService lectureService;
+
+    @Override
+    public QuizResponseDto createQuiz(QuizRequestDto request) {
+        Optional<Lecture> lectureOpt = lectureService.findLectureById(request.getLectureId());
+        if (lectureOpt.isEmpty()) {
+            throw new RuntimeException("Lecture not found");
+        }
+        Lecture lecture = lectureOpt.get();
+
+        Quiz quiz = Quiz.createQuiz(request, lecture);
+        Quiz savedQuiz = quizRepository.save(quiz);
+
+        // Create and save QuizChoice entities
+        List<String> choices = request.getChoices();
+        List<String> answers = request.getAnswers();
+        quizChoiceService.createQuizChoices(choices, answers, savedQuiz);
+
+
+        return QuizResponseDto.createQuizResponseDto(request);
+    }
+
+
+}
