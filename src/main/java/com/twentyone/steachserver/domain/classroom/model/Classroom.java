3f12f53a03be21834c389ce4cdc5c657341fd3a4
sihyunjojo <si4018@naver.com>
2024-07-14 23:42:20 +0900

    refactor : merge complete

--- src/main/java/com/twentyone/steachserver/domain/classroom/model/Classroom.java
+++ src/main/java/com/twentyone/steachserver/domain/classroom/model/Classroom.java
@@ -5,21 +5,20 @@ import com.twentyone.steachserver.domain.lecture.model.Lecture;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Getter(value = AccessLevel.PUBLIC)
-@Setter(value = AccessLevel.PRIVATE)
-@ToString
-@EqualsAndHashCode
+
 @Entity
 @Table(name = "classrooms")
 @NoArgsConstructor
 public class Classroom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     private String sessionId;
 
     @OneToOne
     @JoinColumn(name = "lectures_id")
     private Lecture lecture;
+
+    // Getters and Setters
 }
