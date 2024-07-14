f0fb2feeebd0cfb29421290d63f2dda703e25c07
sihyunjojo <si4018@naver.com>
2024-07-14 16:04:01 +0900

    refactor: Refactor Lecture class to use Lombok for getter methods.

--- src/main/java/com/twentyone/steachserver/domain/lecture/Lecture.java
+++ src/main/java/com/twentyone/steachserver/domain/lecture/Lecture.java
@@ -3,8 +3,12 @@ package com.twentyone.steachserver.domain.lecture;
 
 import com.twentyone.steachserver.domain.Curricula;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
+@Getter(value = AccessLevel.PUBLIC)
 @Entity
 @Table(name = "lectures")
 @NoArgsConstructor
