1e417c5c16cb7a3ce77d387b214452606e281bce
조시현 <si4018@naver.com>
2024-07-15 12:41:17 +0900

    feat: getQuiz 추가 및 createQuiz 수정
    예외를 처리하기 위해 QuizServiceImpl의 createQuiz 메소드를 리팩토링하고 ID별로 퀴즈 응답을 가져오기 위해 QuizController에 getQuizResponseDto 메소드를 추가했습니다.

--- src/main/java/com/twentyone/steachserver/domain/quiz/service/QuizService.java
+++ src/main/java/com/twentyone/steachserver/domain/quiz/service/QuizService.java
@@ -3,8 +3,13 @@ package com.twentyone.steachserver.domain.quiz.service;
 import com.twentyone.steachserver.domain.quiz.dto.QuizRequestDto;
 import com.twentyone.steachserver.domain.quiz.dto.QuizResponseDto;
 
+import java.util.Optional;
+
+
 public interface QuizService {
     // Quiz methods
-    QuizResponseDto createQuiz(QuizRequestDto request);
+    Optional<QuizResponseDto> createQuiz(QuizRequestDto request) throws Exception;
 
+    Optional<QuizResponseDto> getQuizResponseDto(Integer quizId);
 }
+
