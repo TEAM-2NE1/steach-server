b2b74845f6d4b73550b366dba19a5749156a1e79
Juhyorim <joohr1234@gmail.com>
2024-07-16 15:35:57 +0900

    fix: AuthCode 검증코드 추가, 설명 추가
    AuthCode 검증코드 추가, swagger 문서 설명 추가

--- src/main/java/com/twentyone/steachserver/domain/auth/controller/AuthCodeController.java
+++ src/main/java/com/twentyone/steachserver/domain/auth/controller/AuthCodeController.java
@@ -5,6 +5,8 @@ import com.twentyone.steachserver.domain.auth.dto.AuthCodeAuthenticateResponse;
 import com.twentyone.steachserver.domain.auth.dto.AuthCodeRequest;
 import com.twentyone.steachserver.domain.auth.dto.AuthCodeResponse;
 import com.twentyone.steachserver.domain.auth.service.AuthCodeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "AuthCode", description = "AuthCode 관련 API")
 @RequestMapping("/authcode")
 @RequiredArgsConstructor
 @RestController
@@ -23,6 +26,7 @@ public class AuthCodeController {
         return ResponseEntity.ok(authCodeService.createNumberOfAuthCode(authCodeRequest));
     }
 
+    @Operation(summary = "auth코드 발급", description = "무조건 200을 반환, 유효여부는 true false로 반환")
     @PostMapping("/authenticate")
     public ResponseEntity<AuthCodeAuthenticateResponse> authenticate(@RequestBody final AuthCodeAuthenticateRequest request) {
         return ResponseEntity.ok(authCodeService.authenticate(request));
