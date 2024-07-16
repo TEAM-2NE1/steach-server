2742e0179e8ae44fe20000b056e2fd0173902ef7
Harvey <ssafy.lsc@gmail.com>
2024-07-16 13:21:04 +0900

    [BE] PR comment에 따른 주석 추가 S11P12D201-12

--- src/main/java/com/twentyone/steachserver/domain/statistic/service/StatisticsServiceImpl.java
+++ src/main/java/com/twentyone/steachserver/domain/statistic/service/StatisticsServiceImpl.java
@@ -10,6 +10,15 @@ import org.springframework.stereotype.Service;
 public class StatisticsServiceImpl implements StatisticsService{
     private StatisticsRepository statisticsRepository;
 
+    /**
+     * Each item means a focus ratio for one category
+     * (e.g. 76 for Korean, 81 for Math. 98 for Engineering)
+     * Since category names are not settled, just returning 7 numbers.
+     * 각 item은 focus ratio를 의미.
+     * (예시. 국어 76, 수학 81, 공학 98)
+     * 카테고리명이 아직 정해지지 않았기 때문에 숫자 7개의 숫자를 반환.
+     * @return 7 numbers
+     */
     @Override
     public StatisticsDto getStatistics(){
         StatisticsDto statisticsDto = new StatisticsDto();
