d2a5172efa5fd07c8c829246ed6cf6ba72e88195
조시현 <si4018@naver.com>
2024-07-18 11:44:22 +0900

    move: 파일 이동

---
+++ src/main/java/com/twentyone/steachserver/domain/curriculum/enums/CurriculumCategory.java
@@ -0,0 +1,5 @@
+package com.twentyone.steachserver.domain.curriculum.enums;
+
+public enum CurriculumCategory {
+    KOREAN, MATH, FOREIGN_LANGUAGE, SCIENCE, ENGINEERING, ARTS_AND_PHYSICAL, EDUCATION, ETC
+}
