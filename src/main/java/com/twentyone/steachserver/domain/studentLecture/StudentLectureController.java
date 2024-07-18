f051ee78cd8b9e01b41af669f420129779a47fe8
조시현 <si4018@naver.com>
2024-07-18 17:56:17 +0900

    feat: submitTimeFocusTime() 위치 번경

---
+++ src/main/java/com/twentyone/steachserver/domain/studentLecture/StudentLectureController.java
@@ -0,0 +1,24 @@
+package com.twentyone.steachserver.domain.studentLecture;
+
+import com.twentyone.steachserver.domain.classroom.dto.FocusTimeRequestDto;
+import com.twentyone.steachserver.domain.studentLecture.service.StudentLectureService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/v1/studentsLectures")
+@RequiredArgsConstructor
+public class StudentLectureController {
+
+    private final StudentLectureService studentLectureService;
+
+    @PostMapping("/focus-time/{studentId}/{lectureId}")
+    public ResponseEntity<?> submitTimeFocusTime(@PathVariable Integer studentId,
+                                                 @PathVariable Integer lectureId,
+                                                 @RequestBody FocusTimeRequestDto focusTimeDto) {
+        studentLectureService.saveTimeFocusTime(studentId, lectureId, focusTimeDto.getFocusTime());
+        return ResponseEntity.ok().build();
+    }
+}
