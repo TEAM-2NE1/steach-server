5e30b649a117abd3dea2a3d56f5bcd6cd2fe7032
조시현 <si4018@naver.com>
2024-07-18 17:44:21 +0900

    refactor: dto생성 메서드 추가

--- src/main/java/com/twentyone/steachserver/domain/lecture/dto/StudentInfoByLectureDto.java
+++ src/main/java/com/twentyone/steachserver/domain/lecture/dto/StudentInfoByLectureDto.java
@@ -1,13 +1,25 @@
 package com.twentyone.steachserver.domain.lecture.dto;
 
-import com.twentyone.steachserver.domain.quiz.dto.QuizByLectureDto;
+import com.twentyone.steachserver.domain.studentQuiz.dto.StudentQuizDto;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 public class StudentInfoByLectureDto {
-    List<QuizByLectureDto> quizByLectureDtoList;
-    private Integer FocusRatio; // 수업시간 - 졸은 시간 / 100
-    private Integer SleepTime; // minute
+    List<StudentQuizDto> StudentQuizzesDto;
+    private final Integer focusRatio; // 수업시간 - 졸은 시간 / 100
+    private final Integer sleepMinute; // minute
     private Integer totalQuizScore;
     private Integer correctNumber;
+
+    public StudentInfoByLectureDto(List<StudentQuizDto> StudentQuizzesDto, Integer focusRatio, Integer sleepMinute) {
+        this.StudentQuizzesDto = StudentQuizzesDto;
+        for (StudentQuizDto studentQuizDto : StudentQuizzesDto) {
+            this.totalQuizScore += studentQuizDto.getScore();
+            this.correctNumber += studentQuizDto.getScore() == 0 ? 0 : 1;
+        }
+        this.focusRatio = focusRatio;
+        this.sleepMinute = sleepMinute;
+    }
 }
