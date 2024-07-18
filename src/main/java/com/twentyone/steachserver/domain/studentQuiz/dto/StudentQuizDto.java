fc136592903654487ba408552d8c9676c660e4d1
조시현 <si4018@naver.com>
2024-07-18 17:52:38 +0900

    feat: createStudentQuizDto() 추가

--- src/main/java/com/twentyone/steachserver/domain/studentQuiz/dto/StudentQuizDto.java
+++ src/main/java/com/twentyone/steachserver/domain/studentQuiz/dto/StudentQuizDto.java
@@ -1,7 +1,21 @@
 package com.twentyone.steachserver.domain.studentQuiz.dto;
 
+import com.twentyone.steachserver.domain.studentQuiz.model.StudentQuiz;
+import lombok.Getter;
+
+@Getter
 public class StudentQuizDto {
-    private String studentName;
-    private Integer quizScore;
-    private Integer quizNumber;
+    private final String studentName;
+    private final Integer score;
+    private final String student_choice;
+
+    public StudentQuizDto(Integer score, String student_choice, String studentName) {
+        this.studentName = studentName;
+        this.score = score;
+        this.student_choice = student_choice;
+    }
+
+    public static StudentQuizDto createStudentQuizDto(StudentQuiz studentQuiz, String studentName) {
+        return new StudentQuizDto(studentQuiz.getScore(), studentQuiz.getStudentChoice(), studentName);
+    }
 }
