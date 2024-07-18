7e161f900ccd0ce464e96bcac59252ae0403415e
sihyunjojo <si4018@naver.com>
2024-07-18 10:37:49 +0900

    delete: delete Dto

--- src/main/java/com/twentyone/steachserver/domain/studentQuiz/dto/StudentQuizRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.twentyone.steachserver.domain.studentQuiz.dto;
-
-import lombok.Getter;
-
-@Getter
-public class StudentQuizRequestDto {
-    private Integer score;
-    private String student_choice;
-}
