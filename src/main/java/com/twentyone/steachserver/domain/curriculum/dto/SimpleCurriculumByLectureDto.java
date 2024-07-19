54bb5042985e577348a67d99612d4d26e08bb214
sihyunjojo <si4018@naver.com>
2024-07-19 09:53:52 +0900

    style: remove whitespace

--- src/main/java/com/twentyone/steachserver/domain/curriculum/dto/SimpleCurriculumByLectureDto.java
+++ src/main/java/com/twentyone/steachserver/domain/curriculum/dto/SimpleCurriculumByLectureDto.java
@@ -3,7 +3,7 @@ import com.twentyone.steachserver.domain.curriculum.model.Curriculum;
 
 public record SimpleCurriculumByLectureDto(String curriculumTitle,String curriculumCategory) {
 
-    public static  SimpleCurriculumByLectureDto createSimpleCurriculumByLectureDto(Curriculum curriculum) {
+    public static SimpleCurriculumByLectureDto createSimpleCurriculumByLectureDto(Curriculum curriculum) {
         return new SimpleCurriculumByLectureDto(curriculum.getTitle(), curriculum.getCategory().toString());
     }
 }
