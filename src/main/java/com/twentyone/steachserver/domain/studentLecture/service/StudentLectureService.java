52ca5d474f446cebad449dc1d65a221770de2638
sihyunjojo <si4018@naver.com>
2024-07-19 09:56:00 +0900

    feat: 필요한 로직 추가 및 필요 없는 로직 삭제

--- src/main/java/com/twentyone/steachserver/domain/studentLecture/service/StudentLectureService.java
+++ src/main/java/com/twentyone/steachserver/domain/studentLecture/service/StudentLectureService.java
@@ -1,20 +1,16 @@
 package com.twentyone.steachserver.domain.studentLecture.service;
 
-import com.twentyone.steachserver.domain.lecture.dto.StudentInfoByLectureDto;
-import com.twentyone.steachserver.domain.lecture.model.Lecture;
+import com.twentyone.steachserver.domain.studentLecture.dto.StudentLectureStatisticDto;
 import com.twentyone.steachserver.domain.studentLecture.model.StudentLecture;
-import com.twentyone.steachserver.domain.studentLecture.model.StudentLecture;
-import com.twentyone.steachserver.domain.member.model.Student;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface StudentLectureService {
     Optional<StudentLecture> findByStudentIdAndLectureId(Integer studentId, Integer lectureId);
-    List<StudentInfoByLectureDto> findAllStudentInfoByLectureId(Integer lectureId);
-    void createAndSaveStudentLecture(Student student, Lecture lecture, Integer focusTime);
 
     void saveTimeFocusTime(Integer studentId, Integer lectureId, Integer focusTime);
 
+    void updateStudentLectureByFinishLecture(Integer lectureId);
 
+    StudentLectureStatisticDto createLectureStudentStatistic(Integer lectureId);
 }
