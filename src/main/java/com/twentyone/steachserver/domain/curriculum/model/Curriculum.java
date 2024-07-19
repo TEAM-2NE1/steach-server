8c1dae3d7593422372d7c4334bd526f838fadc67
sihyunjojo <si4018@naver.com>
2024-07-19 09:49:20 +0900

    refactor: 빠진 로직 추가

--- src/main/java/com/twentyone/steachserver/domain/curriculum/model/Curriculum.java
+++ src/main/java/com/twentyone/steachserver/domain/curriculum/model/Curriculum.java
@@ -1,11 +1,16 @@
 package com.twentyone.steachserver.domain.curriculum.model;
 
+import com.twentyone.steachserver.domain.lecture.model.Lecture;
+import com.twentyone.steachserver.domain.member.model.Student;
 import com.twentyone.steachserver.domain.member.model.Teacher;
 import com.twentyone.steachserver.domain.curriculum.enums.CurriculumCategory;
+import com.twentyone.steachserver.domain.studentCurriculum.model.StudentCurriculum;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name = "curricula")
@@ -22,6 +27,14 @@ public class Curriculum {
     @ManyToOne
     @JoinColumn(name = "teacher_id")
     private Teacher teacher;
+
+    @OneToMany(mappedBy = "curriculum")
+    private List<Lecture> lectures;
+
+
+    @OneToMany(mappedBy = "curriculum")
+    private List<StudentCurriculum> studentCurricula;
+
 
     @Enumerated(EnumType.STRING)
     private CurriculumCategory category;
