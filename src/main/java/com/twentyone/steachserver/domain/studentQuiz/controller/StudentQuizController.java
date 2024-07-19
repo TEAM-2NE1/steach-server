482961a1ea5836a100154d5450a12c3b1ba189ab
조시현 <si4018@naver.com>
2024-07-19 16:34:19 +0900

    refactor: 반환값 추가

--- src/main/java/com/twentyone/steachserver/domain/studentQuiz/controller/StudentQuizController.java
+++ src/main/java/com/twentyone/steachserver/domain/studentQuiz/controller/StudentQuizController.java
@@ -1,6 +1,7 @@
 package com.twentyone.steachserver.domain.studentQuiz.controller;
 
 import com.twentyone.steachserver.domain.studentQuiz.dto.StudentQuizRequestDto;
+import com.twentyone.steachserver.domain.studentQuiz.model.StudentQuiz;
 import com.twentyone.steachserver.domain.studentQuiz.service.StudentQuizService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,10 +16,10 @@ public class StudentQuizController {
     private final StudentQuizService studentQuizService;
 
     @PostMapping("/{studentId}/{quizId}")
-    public ResponseEntity<?> createStudentQuiz(@PathVariable Integer studentId, @PathVariable Integer quizId, @RequestBody StudentQuizRequestDto requestDto) throws Exception {
-        studentQuizService.createStudentQuiz(studentId, quizId, requestDto);
+    public ResponseEntity<?> createStudentQuiz(@PathVariable("studentId") Integer studentId, @PathVariable("quizId") Integer quizId, @RequestBody StudentQuizRequestDto requestDto) throws Exception {
+        StudentQuiz studentQuiz = studentQuizService.createStudentQuiz(studentId, quizId, requestDto);
         return ResponseEntity
-                .status(HttpStatus.OK).build();
+                .status(HttpStatus.OK).body(studentQuiz);
     }
 
 }
