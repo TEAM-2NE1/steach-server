170814eb57e551d2356b0baf852d60196034a30c
sihyunjojo <si4018@naver.com>
2024-07-19 09:59:34 +0900

    refactor: totalFocusRatio 타입 변경

--- src/main/java/com/twentyone/steachserver/domain/lecture/dto/FinalLectureInfoByTeacherDto.java
+++ src/main/java/com/twentyone/steachserver/domain/lecture/dto/FinalLectureInfoByTeacherDto.java
@@ -1,12 +1,14 @@
 package com.twentyone.steachserver.domain.lecture.dto;
 import lombok.Getter;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 @Getter
 public class FinalLectureInfoByTeacherDto {
     private final List<StudentInfoByLectureDto> studentInfoByLectureDtoList;
-    private Integer averageFocusRatio;
+    private BigDecimal averageFocusRatio;
     private Integer averageSleepMinute;
     private Integer averageQuizScore;
     private Integer averageCorrectNumber;
@@ -18,22 +20,22 @@ public class FinalLectureInfoByTeacherDto {
     public static FinalLectureInfoByTeacherDto createFinalLectureInfoByTeacherDto(List<StudentInfoByLectureDto> studentInfoByLectureDtoList) {
         FinalLectureInfoByTeacherDto finalLectureInfoByTeacherDto = new FinalLectureInfoByTeacherDto(studentInfoByLectureDtoList);
 
-        int totalFocusRatio = 0;
-        int totalSleepMinute = 0;
+        BigDecimal totalFocusRatio = BigDecimal.valueOf(0);
+        int totalFocusMinute = 0;
         int totalQuizScore = 0;
         int totalCorrectNumber = 0;
 
         for (StudentInfoByLectureDto studentInfo : studentInfoByLectureDtoList) {
-            totalFocusRatio += studentInfo.getFocusRatio();
-            totalSleepMinute += studentInfo.getSleepMinute();
+            totalFocusRatio = totalFocusRatio.add(studentInfo.getFocusRatio());
+            totalFocusMinute += studentInfo.getFocusMinute();
             totalQuizScore += studentInfo.getTotalQuizScore();
             totalCorrectNumber += studentInfo.getCorrectNumber();
         }
 
         int studentCount = studentInfoByLectureDtoList.size();
 
-        finalLectureInfoByTeacherDto.averageFocusRatio = studentCount > 0 ? totalFocusRatio / studentCount : 0;
-        finalLectureInfoByTeacherDto.averageSleepMinute = studentCount > 0 ? totalSleepMinute / studentCount : 0;
+        finalLectureInfoByTeacherDto.averageFocusRatio = studentCount > 0 ? totalFocusRatio.divide(BigDecimal.valueOf(studentCount), 2, RoundingMode.HALF_UP) : BigDecimal.ZERO;
+        finalLectureInfoByTeacherDto.averageSleepMinute = studentCount > 0 ? totalFocusMinute / studentCount : 0;
         finalLectureInfoByTeacherDto.averageQuizScore = studentCount > 0 ? totalQuizScore / studentCount : 0;
         finalLectureInfoByTeacherDto.averageCorrectNumber = studentCount > 0 ? totalCorrectNumber / studentCount : 0;
 
