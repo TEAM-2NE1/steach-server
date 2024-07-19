9333800c23f1049ec4b44c320cc93e9e58c8ffb4
조시현 <si4018@naver.com>
2024-07-19 16:35:22 +0900

    refactor: 권한 삭제

--- src/main/java/com/twentyone/steachserver/domain/lecture/service/LectureService.java
+++ src/main/java/com/twentyone/steachserver/domain/lecture/service/LectureService.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface LectureService {
-    public Optional<Lecture> findLectureById(Integer id);
+    Optional<Lecture> findLectureById(Integer id);
 
     List<Lecture> upcomingLecture(int toMinute, int fromMinute);
 
