36b6978e2a48a9c8d1c3841fa95151e3f9de2066
sihyunjojo <si4018@naver.com>
2024-07-19 09:47:09 +0900

    refactor: 변수 추가 및 생성 로직 추가

--- src/main/java/com/twentyone/steachserver/domain/lecture/dto/CompletedLecturesResponseDto.java
+++ src/main/java/com/twentyone/steachserver/domain/lecture/dto/CompletedLecturesResponseDto.java
@@ -1,24 +1,38 @@
 package com.twentyone.steachserver.domain.lecture.dto;
 
-import com.twentyone.steachserver.domain.curriculum.dto.CurriculumDetailByLectureDto;
-import com.twentyone.steachserver.domain.curriculum.dto.SimpleCurriculumByLectureDto;
 import com.twentyone.steachserver.domain.lecture.model.Lecture;
-import com.twentyone.steachserver.domain.member.dto.StudentByLectureDto;
-import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CompletedLecturesResponseDto extends LectureBeforeStartingResponseDto{
-//    private LectureBeforeStartingResponseDto lectureBeforeStartingResponseDto;
-
-    private final List<StudentInfoByLectureDto> studentsQuizzesByLectureDto = new ArrayList<>();
+    private LectureBeforeStartingResponseDto lectureBeforeStartingResponseDto;
+    private List<StudentInfoByLectureDto> studentsQuizzesByLectureDto;
 
     private LocalDateTime realStartTime;
     private LocalDateTime realEndTime;
 
-    public CompletedLecturesResponseDto(Lecture lecture, SimpleCurriculumByLectureDto curriculumInfo, CurriculumDetailByLectureDto curriculumDetailInfo, List<StudentByLectureDto> studentDto) {
-        super(lecture, curriculumInfo, curriculumDetailInfo, studentDto);
+    private final Boolean isCompleted = true;
+
+    protected CompletedLecturesResponseDto() {
+        super();
+    };
+
+    private CompletedLecturesResponseDto(LectureBeforeStartingResponseDto lectureBeforeStartingResponseDto,
+                                         List<StudentInfoByLectureDto> studentsQuizzesByLectureDto,
+                                         LocalDateTime realStartTime, LocalDateTime realEndTime) {
+        this.lectureBeforeStartingResponseDto = lectureBeforeStartingResponseDto;
+        this.studentsQuizzesByLectureDto = studentsQuizzesByLectureDto;
+        this.realStartTime = realStartTime;
+        this.realEndTime = realEndTime;
     }
+
+    public static CompletedLecturesResponseDto of(LectureBeforeStartingResponseDto lectureBeforeStartingResponseDto,
+                    List<StudentInfoByLectureDto> studentsQuizzesByLectureDto,
+                    Lecture lecture) {
+        return new CompletedLecturesResponseDto(lectureBeforeStartingResponseDto, studentsQuizzesByLectureDto, lecture.getRealStartTime(),lecture.getRealEndTime());
+    }
+
 }
