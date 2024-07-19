eb420fa72a580c54306d89f10e2cf302c49639b5
sihyunjojo <si4018@naver.com>
2024-07-19 09:50:36 +0900

    refactor: 생성 로직 객체로 파라미터 변경

--- src/main/java/com/twentyone/steachserver/domain/member/dto/StudentByLectureDto.java
+++ src/main/java/com/twentyone/steachserver/domain/member/dto/StudentByLectureDto.java
@@ -1,12 +1,13 @@
 package com.twentyone.steachserver.domain.member.dto;
 
+import com.twentyone.steachserver.domain.member.model.Student;
 import com.twentyone.steachserver.domain.studentQuiz.dto.StudentQuizDto;
 import com.twentyone.steachserver.domain.studentQuiz.model.StudentQuiz;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 public record StudentByLectureDto(String name, String email) {
-    public static StudentByLectureDto createStudentByLectureDto(String name, String email) {
-        return new StudentByLectureDto(name, email);
+    public static StudentByLectureDto createStudentByLectureDto(Student student) {
+        return new StudentByLectureDto(student.getName(), student.getEmail());
     }
 }
