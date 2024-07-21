0a856ca93cb47ef3c7090c9e22b0f7938be0d110
sihyunjojo <si4018@naver.com>
2024-07-21 15:19:10 +0900

    refactor: 메서드 다른 클래스로 이동

--- src/main/java/com/twentyone/steachserver/domain/studentLecture/service/StudentLectureService.java
+++ src/main/java/com/twentyone/steachserver/domain/studentLecture/service/StudentLectureService.java
@@ -1,16 +1,12 @@
 package com.twentyone.steachserver.domain.studentLecture.service;
 
-import com.twentyone.steachserver.domain.studentLecture.dto.StudentLectureStatisticDto;
 import com.twentyone.steachserver.domain.studentLecture.model.StudentLecture;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentLectureService {
-    Optional<StudentLecture> findByStudentIdAndLectureId(Integer studentId, Integer lectureId);
-
     void saveTimeFocusTime(Integer studentId, Integer lectureId, Integer focusTime);
 
     void updateStudentLectureByFinishLecture(Integer lectureId);
-
-    StudentLectureStatisticDto createLectureStudentStatistic(Integer lectureId);
 }
--- src/main/java/com/twentyone/steachserver/domain/studentLecture/service/StudentLectureServiceImpl.java
+++ src/main/java/com/twentyone/steachserver/domain/studentLecture/service/StudentLectureServiceImpl.java
@@ -3,9 +3,8 @@ package com.twentyone.steachserver.domain.studentLecture.service;
 import com.twentyone.steachserver.domain.lecture.model.Lecture;
 import com.twentyone.steachserver.domain.lecture.repository.LectureRepository;
 import com.twentyone.steachserver.domain.member.repository.StudentRepository;
-import com.twentyone.steachserver.domain.studentLecture.dto.StudentLectureStatisticDto;
 import com.twentyone.steachserver.domain.studentLecture.model.StudentLecture;
-import com.twentyone.steachserver.domain.studentLecture.repository.StudentLectureMongoRepository;
+import com.twentyone.steachserver.domain.statistic.repository.LectureStatisticMongoRepository;
 import com.twentyone.steachserver.domain.studentLecture.repository.StudentLectureQueryRepository;
 import com.twentyone.steachserver.domain.studentLecture.repository.StudentLectureRepository;
 import com.twentyone.steachserver.domain.member.model.Student;
@@ -13,9 +12,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,31 +19,20 @@ import java.util.Optional;
 public class StudentLectureServiceImpl implements StudentLectureService {
 
     private final StudentLectureRepository studentLectureRepository;
-    private final StudentLectureMongoRepository studentLectureMongoRepository;
+    private final LectureStatisticMongoRepository studentLectureMongoRepository;
     private final StudentLectureQueryRepository studentLectureQueryRepository;
 
     private final LectureRepository lectureRepository;
     private final StudentRepository studentRepository;
 
     @Override
-    public Optional<StudentLecture> findByStudentIdAndLectureId(Integer studentId, Integer lectureId) {
-        return studentLectureRepository.findByStudentIdAndLectureId(studentId, lectureId);
-    }
-
-    @Override
     @Transactional
     public void saveTimeFocusTime(Integer studentId, Integer lectureId, Integer focusTime) {
-        Optional<StudentLecture> byStudentIdAndLectureId = findByStudentIdAndLectureId(studentId, lectureId);
-
-        // 존재하면 더 하고
-        if (byStudentIdAndLectureId.isPresent()) {
-            StudentLecture studentLecture = byStudentIdAndLectureId.get();
-            studentLecture.sumFocusTime(focusTime);
-            return;
-        }
-
-        // 존재하지 않으면 새로 만들어서 넣어주고
-        createAndSaveStudentLecture(studentId, lectureId, focusTime);
+        studentLectureRepository.findByStudentIdAndLectureId(studentId, lectureId)
+                .ifPresentOrElse(
+                        studentLecture -> studentLecture.sumFocusTime(focusTime),
+                        () -> createAndSaveStudentLecture(studentId, lectureId, focusTime)
+                );
     }
 
     private void createAndSaveStudentLecture(Integer studentId, Integer lectureId, Integer focusTime) {
@@ -61,40 +46,5 @@ public class StudentLectureServiceImpl implements StudentLectureService {
     @Transactional
     public void updateStudentLectureByFinishLecture(Integer lectureId) {
         studentLectureQueryRepository.updateStudentLectureByFinishLecture(lectureId);
-    }
-
-    @Override
-    @Transactional
-    public StudentLectureStatisticDto createLectureStudentStatistic(Integer lectureId) {
-        List<StudentLecture> allStudentInfoByLectureId = findAllStudentInfoByLectureId(lectureId);
-
-        int studentCount = allStudentInfoByLectureId.size();
-
-        Integer totalQuizTotalScore = 0;
-        Integer totalQuizAnswerCount = 0;
-        Integer totalFocusTime = 0;
-        BigDecimal totalFocusRatio = BigDecimal.valueOf(0);
-
-
-        for (StudentLecture studentLecture : allStudentInfoByLectureId) {
-            totalQuizTotalScore += studentLecture.getQuizTotalScore();
-            totalQuizAnswerCount += studentLecture.getQuizAnswerCount();
-            totalFocusRatio = totalFocusRatio.add(studentLecture.getFocusRatio());
-            totalFocusTime += studentLecture.getFocusTime();
-        }
-
-        Integer averageQuizTotalScore = totalQuizTotalScore/studentCount;
-        Integer averageQuizAnswerCount = totalQuizAnswerCount/studentCount;
-        Integer averageFocusTime = totalFocusTime/studentCount;
-        BigDecimal averageFocusRatio = totalFocusRatio.divide(BigDecimal.valueOf(studentCount), 2, RoundingMode.HALF_UP);
-
-        // 몽고디비 저장 나중에 몽고디비 연결해서 해야함.
-        StudentLectureStatisticDto lectureStudentStatisticDto = StudentLectureStatisticDto.of(averageQuizTotalScore, averageQuizAnswerCount, averageFocusTime, averageFocusRatio);
-        studentLectureMongoRepository.save(lectureStudentStatisticDto);
-        return lectureStudentStatisticDto;
-    }
-
-    private List<StudentLecture> findAllStudentInfoByLectureId(Integer lectureId) {
-        return studentLectureQueryRepository.findAllStudentInfoByLectureId(lectureId);
     }
 }
