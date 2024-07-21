796e8fd4ec7336dc9ff8ce72797623b3ad3ad166
sihyunjojo <si4018@naver.com>
2024-07-21 17:41:34 +0900

    refactor: 객체명 명확하게 변경 + 예외처리 컨트롤러에서 하게 변경

--- src/main/java/com/twentyone/steachserver/domain/statistic/controller/StatisticController.java
+++ src/main/java/com/twentyone/steachserver/domain/statistic/controller/StatisticController.java
@@ -2,7 +2,7 @@ package com.twentyone.steachserver.domain.statistic.controller;
 
 import com.twentyone.steachserver.domain.member.model.Student;
 import com.twentyone.steachserver.domain.statistic.dto.LectureStatisticsByAllStudentDto;
-import com.twentyone.steachserver.domain.statistic.dto.StatisticsDto;
+import com.twentyone.steachserver.domain.statistic.dto.radarChartStatisticDto;
 import com.twentyone.steachserver.domain.statistic.dto.GPTDataRequestDto;
 import com.twentyone.steachserver.domain.statistic.service.StatisticService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -20,8 +20,8 @@ public class StatisticController {
 
     @Operation(summary = "통계 정보 반환 ", description = "무조건 200을 반환")
     @GetMapping
-    public ResponseEntity<?> getStatistic(@AuthenticationPrincipal Student student) {
-        StatisticsDto statistics = statisticService.getStatistics(student.getId());
+    public ResponseEntity<radarChartStatisticDto> getStatistic(@AuthenticationPrincipal Student student) {
+        radarChartStatisticDto statistics = statisticService.getStatistics(student.getId());
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(statistics);
@@ -29,7 +29,7 @@ public class StatisticController {
 
     @Operation(summary = "GPT 문장 반환 ", description = "무조건 200을 반환")
     @GetMapping("/gpt")
-    public ResponseEntity<?> getGPTStatistic(@AuthenticationPrincipal Student student, @RequestBody GPTDataRequestDto gptDataRequestDto) {
+    public ResponseEntity<String> getGPTStatistic(@AuthenticationPrincipal Student student, @RequestBody GPTDataRequestDto gptDataRequestDto) {
         String gptString = statisticService.createGPTString(student, gptDataRequestDto);
         return ResponseEntity.ok()
                 .body(gptString);
@@ -37,9 +37,9 @@ public class StatisticController {
 
     @Operation(summary = "강의에 대한 전체 학생에 대한 통계 반환 ", description = "무조건 200을 반환")
     @GetMapping("/lecture/{lectureId}")
-    public ResponseEntity<?> getLectureStatisticsByAllStudent(@PathVariable Integer lectureId) {
-        LectureStatisticsByAllStudentDto statistics = statisticService.getLectureStatisticsByAllStudent(lectureId);
-        return ResponseEntity.ok()
-                .body(statistics);
+    public ResponseEntity<LectureStatisticsByAllStudentDto> getLectureStatisticsByAllStudent(@PathVariable Integer lectureId) {
+        return statisticService.getLectureStatisticsByAllStudent(lectureId)
+                .map(ls -> ResponseEntity.ok().body(LectureStatisticsByAllStudentDto.of(ls)))
+                .orElseThrow(() -> new IllegalArgumentException("lectureId : " + lectureId + " does not exist"));
     }
 }
--- src/main/java/com/twentyone/steachserver/domain/statistic/service/StatisticService.java
+++ src/main/java/com/twentyone/steachserver/domain/statistic/service/StatisticService.java
@@ -2,16 +2,18 @@ package com.twentyone.steachserver.domain.statistic.service;
 
 import com.twentyone.steachserver.domain.lecture.model.Lecture;
 import com.twentyone.steachserver.domain.member.model.Student;
-import com.twentyone.steachserver.domain.statistic.dto.LectureStatisticsByAllStudentDto;
-import com.twentyone.steachserver.domain.statistic.dto.StatisticsDto;
+import com.twentyone.steachserver.domain.statistic.dto.radarChartStatisticDto;
 import com.twentyone.steachserver.domain.statistic.dto.GPTDataRequestDto;
+import com.twentyone.steachserver.domain.statistic.model.LectureStatisticsByAllStudent;
+
+import java.util.Optional;
 
 public interface StatisticService {
-    StatisticsDto getStatistics(Integer studentId);
+    radarChartStatisticDto getStatistics(Integer studentId);
 
     String createGPTString(Student student, GPTDataRequestDto gptDataRequestDto);
 
-    LectureStatisticsByAllStudentDto getLectureStatisticsByAllStudent(Integer lectureId);
+    Optional<LectureStatisticsByAllStudent> getLectureStatisticsByAllStudent(Integer lectureId);
 
     void createStatisticsByFinalLecture(Lecture lecture);
 }
