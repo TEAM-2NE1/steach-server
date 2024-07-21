6650f7287b3a2e6cbbe5ecb6e77d504cb596e3fa
sihyunjojo <si4018@naver.com>
2024-07-21 17:41:04 +0900

    refactor: 객체명 명확하게 변경

--- src/main/java/com/twentyone/steachserver/domain/statistic/repository/StatisticRepository.java
+++
@@ -1,9 +0,0 @@
-package com.twentyone.steachserver.domain.statistic.repository;
-
-import com.twentyone.steachserver.domain.statistic.model.Statistic;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface StatisticRepository extends JpaRepository<Statistic, Integer> {
-}
