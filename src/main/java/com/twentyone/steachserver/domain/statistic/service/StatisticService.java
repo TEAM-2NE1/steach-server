835bd39a0c469f88807b53e6b841f72fa95031fd
sihyunjojo <si4018@naver.com>
2024-07-21 17:20:07 +0900

    refactor: 통계 로직 변경 및 위치 변경

--- src/main/java/com/twentyone/steachserver/domain/statistic/service/StatisticService.java
+++ src/main/java/com/twentyone/steachserver/domain/statistic/service/StatisticService.java
@@ -1,13 +1,17 @@
 package com.twentyone.steachserver.domain.statistic.service;
 
 import com.twentyone.steachserver.domain.lecture.model.Lecture;
+import com.twentyone.steachserver.domain.member.model.Student;
+import com.twentyone.steachserver.domain.statistic.dto.LectureStatisticsByAllStudentDto;
 import com.twentyone.steachserver.domain.statistic.dto.StatisticsDto;
-import com.twentyone.steachserver.domain.studentLecture.model.StudentLecture;
-
-import java.util.List;
+import com.twentyone.steachserver.domain.statistic.dto.GPTDataRequestDto;
 
 public interface StatisticService {
     StatisticsDto getStatistics(Integer studentId);
+
+    String createGPTString(Student student, GPTDataRequestDto gptDataRequestDto);
+
+    LectureStatisticsByAllStudentDto getLectureStatisticsByAllStudent(Integer lectureId);
 
     void createStatisticsByFinalLecture(Lecture lecture);
 }
--- src/main/java/com/twentyone/steachserver/domain/statistic/service/StatisticServiceImpl.java
+++ src/main/java/com/twentyone/steachserver/domain/statistic/service/StatisticServiceImpl.java
@@ -3,14 +3,16 @@ package com.twentyone.steachserver.domain.statistic.service;
 import com.twentyone.steachserver.domain.curriculum.model.Curriculum;
 import com.twentyone.steachserver.domain.curriculum.repository.CurriculumRepository;
 import com.twentyone.steachserver.domain.lecture.model.Lecture;
-import com.twentyone.steachserver.domain.lecture.repository.LectureRepository;
 import com.twentyone.steachserver.domain.member.model.Student;
+import com.twentyone.steachserver.domain.statistic.dto.LectureStatisticsByAllStudentDto;
 import com.twentyone.steachserver.domain.statistic.dto.StatisticsDto;
+import com.twentyone.steachserver.domain.statistic.dto.GPTDataRequestDto;
 import com.twentyone.steachserver.domain.statistic.model.GPTDataByLecture;
 import com.twentyone.steachserver.domain.statistic.model.LectureStatisticsByAllStudent;
-import com.twentyone.steachserver.domain.statistic.model.LectureStatisticsByStudent;
+import com.twentyone.steachserver.domain.statistic.dto.temp.LectureStatisticsByStudentDto;
 import com.twentyone.steachserver.domain.statistic.repository.GPTDataByLectureMongoRepository;
 import com.twentyone.steachserver.domain.statistic.repository.LectureStatisticMongoRepository;
+import com.twentyone.steachserver.domain.statistic.repository.StatisticRepository;
 import com.twentyone.steachserver.domain.studentLecture.model.StudentLecture;
 import com.twentyone.steachserver.domain.studentLecture.repository.StudentLectureQueryRepository;
 import jakarta.transaction.Transactional;
@@ -20,15 +22,14 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class StatisticServiceImpl implements StatisticService{
-    private final LectureRepository lectureRepository;
     private final CurriculumRepository curriculumRepository;
     private final StudentLectureQueryRepository studentLectureQueryRepository;
 
+    private final StatisticRepository statisticRepository;
     private final LectureStatisticMongoRepository lectureStatisticMongoRepository;
     private final GPTDataByLectureMongoRepository gptDataByLectureMongoRepository;
 
@@ -48,27 +49,29 @@ public class StatisticServiceImpl implements StatisticService{
      * @return 7 numbers
      */
 
+
+// HACK : 응급조치로 우선 하드코딩을
     @Override
-    public StatisticsDto getStatistics(Integer studentId){
+    public StatisticsDto getStatistics(Integer studentId) {
         StatisticsDto statisticsDto = new StatisticsDto();
 
         double[] listAvgFocusRatio = new double[NUMBER_OF_CATEGORIES];
         int[] listLectureMinutes = new int[NUMBER_OF_CATEGORIES];
 
         /*
-        * 여기에 students_statistics 23개의 column을 가져오는 코드 작성
-        * */
+         * 여기에 students_statistics 23개의 column을 가져오는 코드 작성
+         * */
 
         double maxFocusRatio = -1;
         int maxLectureMinutes = -1;
 
         int[] retStatistics = new int[NUMBER_OF_CATEGORIES];
 
-        for(int i = 0; i < NUMBER_OF_CATEGORIES; i++){
-            if(maxFocusRatio > listAvgFocusRatio[i]){
+        for (int i = 0; i < NUMBER_OF_CATEGORIES; i++) {
+            if (maxFocusRatio > listAvgFocusRatio[i]) {
                 maxFocusRatio = listAvgFocusRatio[i];
             }
-            if(maxLectureMinutes > listLectureMinutes[i]){
+            if (maxLectureMinutes > listLectureMinutes[i]) {
                 maxLectureMinutes = listLectureMinutes[i];
             }
         }
@@ -86,8 +89,8 @@ public class StatisticServiceImpl implements StatisticService{
         double factorWeightingLectureMinutes = ((double) WEIGHT_LECTURE_MINUTES / maxLectureMinutes);
 
 
-        for(int i = 0; i < NUMBER_OF_CATEGORIES; i++){
-            retStatistics[i] = (int)((listAvgFocusRatio[i] * factorWeightingFocusRatio) + (listLectureMinutes[i] * factorWeightingLectureMinutes));
+        for (int i = 0; i < NUMBER_OF_CATEGORIES; i++) {
+            retStatistics[i] = (int) ((listAvgFocusRatio[i] * factorWeightingFocusRatio) + (listLectureMinutes[i] * factorWeightingLectureMinutes));
         }
 
         // 카테고리가 현행 7개 이므로 하드코딩(...)
@@ -99,7 +102,44 @@ public class StatisticServiceImpl implements StatisticService{
         statisticsDto.setItem6(retStatistics[5]);
         statisticsDto.setItem7(retStatistics[6]);
 
+        // 저장 부탁드릴게요!
+//        statisticRepository.save(statistics);
+
         return statisticsDto;
+    }
+
+    @Override
+    public String createGPTString(Student student, GPTDataRequestDto gptDataRequestDto) {
+//        XXX: 영어로 작성했으면 좋겠어요!
+        StringBuilder sb = new StringBuilder();
+        sb.append("I want career recommendations.");
+
+
+        List<Integer> lectureIds = gptDataRequestDto.lectureIds();
+        for (Integer lectureId : lectureIds) {
+            GPTDataByLecture statistic = getGPTStatistic(lectureId);
+
+//            TODO:  로직 만들어야함 !!!!
+            sb.append("\n");
+            sb.append(statistic);
+        }
+
+        sb.append("I hope the answer begins with ").append(student.getName()).append("’s career recommendation results.");
+        sb.append("in korean");
+        return sb.toString();
+    }
+
+    private GPTDataByLecture getGPTStatistic(Integer lectureId) {
+        return gptDataByLectureMongoRepository.findByLectureId(lectureId)
+                .orElseThrow(() -> new IllegalArgumentException("lectureId : " + lectureId + " does not exist"));
+
+    }
+
+    @Override
+    public LectureStatisticsByAllStudentDto getLectureStatisticsByAllStudent(Integer lectureId) {
+        return lectureStatisticMongoRepository.findByLectureId(lectureId)
+                .map(LectureStatisticsByAllStudentDto::of)
+                .orElseThrow(() -> new IllegalArgumentException("lectureId : " + lectureId + " does not exist"));
     }
 
     @Override
@@ -111,8 +151,9 @@ public class StatisticServiceImpl implements StatisticService{
     }
 
 
+    // 음.. 먼진 모르겠지만 protected 써야지 Trancsacion 사용가능했음.
     @Transactional
-    public void createLectureStatisticsByAllStudent(Lecture lecture, List<StudentLecture> allStudentInfoByLectureId) {
+    protected void createLectureStatisticsByAllStudent(Lecture lecture, List<StudentLecture> allStudentInfoByLectureId) {
         int studentCount = allStudentInfoByLectureId.size();
 
         Integer totalQuizTotalScore = 0;
@@ -137,22 +178,23 @@ public class StatisticServiceImpl implements StatisticService{
         lectureStatisticMongoRepository.save(lectureStatisticsByAllStudent);
     }
 
-    private void createGPTData(Lecture lecture, List<StudentLecture> allStudentInfoByLectureId) {
+    @Transactional
+    protected void createGPTData(Lecture lecture, List<StudentLecture> allStudentInfoByLectureId) {
         Curriculum curriculum = curriculumRepository.findByLecturesContaining(lecture)
                 .orElseThrow(() -> new IllegalStateException("curriculum not found"));
 
         // 학생별로 데이터를 모으기 위한 맵
-        Map<Integer, LectureStatisticsByStudent> studentStatisticsMap = new HashMap<>();
+        Map<Integer, LectureStatisticsByStudentDto> studentStatisticsMap = new HashMap<>();
 
         for (StudentLecture studentLecture : allStudentInfoByLectureId) {
             Student student = studentLecture.getStudent();
 
             studentStatisticsMap
-                    .computeIfAbsent(student.getId(), k -> LectureStatisticsByStudent.of(student, lecture))
+                    .computeIfAbsent(student.getId(), k -> LectureStatisticsByStudentDto.of(student, lecture))
                     .addLectureData(studentLecture);
         }
 
-        for (LectureStatisticsByStudent lectureStatisticsByStudent : studentStatisticsMap.values()) {
+        for (LectureStatisticsByStudentDto lectureStatisticsByStudent : studentStatisticsMap.values()) {
             GPTDataByLecture gptDataByLecture = GPTDataByLecture.of(lecture, curriculum, lectureStatisticsByStudent);
             gptDataByLectureMongoRepository.save(gptDataByLecture);
         }
