e37fc3b4f650288b496e0312640e4238cbaf5d64
sihyunjojo <si4018@naver.com>
2024-07-21 15:33:15 +0900

    refactor: 메서드 위치 변경에 따른 변경

--- src/main/java/com/twentyone/steachserver/domain/lecture/controller/LectureController.java
+++ src/main/java/com/twentyone/steachserver/domain/lecture/controller/LectureController.java
@@ -3,9 +3,10 @@ package com.twentyone.steachserver.domain.lecture.controller;
 import com.twentyone.steachserver.domain.lecture.dto.CompletedLecturesResponseDto;
 import com.twentyone.steachserver.domain.lecture.dto.FinalLectureInfoByTeacherDto;
 import com.twentyone.steachserver.domain.lecture.dto.LectureBeforeStartingResponseDto;
-import com.twentyone.steachserver.domain.lecture.dto.UpdateLectureRequestDto;
+import com.twentyone.steachserver.domain.lecture.dto.update.UpdateLectureRequestDto;
+import com.twentyone.steachserver.domain.lecture.model.Lecture;
 import com.twentyone.steachserver.domain.lecture.service.LectureService;
-import com.twentyone.steachserver.domain.studentLecture.dto.StudentLectureStatisticDto;
+import com.twentyone.steachserver.domain.statistic.service.StatisticService;
 import com.twentyone.steachserver.domain.studentLecture.service.StudentLectureService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,7 @@ public class LectureController {
 
     private final LectureService lectureService;
     private final StudentLectureService studentLectureService;
+    private final StatisticService statisticService;
 
     @Operation(summary = "강의에 대한 다양한 정보 반환", description = "무조건 200을 반환, 강의에 대해서 시작 전 강의면 시작 전 형태로, 끝난 강의는 끝난형태로 반환")
     @GetMapping("/{lectureId}")
@@ -38,10 +40,10 @@ public class LectureController {
     @Operation(summary = "선생님이 강의를 끝내고 관련 정보 처리 및 최종정보 반환 ", description = "무조건 200을 반환")
     @GetMapping("/final/{lectureId}")
     public ResponseEntity<FinalLectureInfoByTeacherDto> getFinalLectureInformation(@PathVariable("lectureId") Integer lectureId) {
-        lectureService.updateRealEndTime(lectureId);
+        Lecture updateLecture = lectureService.updateRealEndTime(lectureId);
         studentLectureService.updateStudentLectureByFinishLecture(lectureId);
-        StudentLectureStatisticDto lectureStudentStatistic = studentLectureService.createLectureStudentStatistic(lectureId);
-        lectureService.createGPTData(lectureId, lectureStudentStatistic);
+
+        statisticService.createStatisticsByFinalLecture(updateLecture);
         FinalLectureInfoByTeacherDto finalLectureInfoByTeacherDto = lectureService.getFinalLectureInformation(lectureId);
         return ResponseEntity.ok().body(finalLectureInfoByTeacherDto);
     }
