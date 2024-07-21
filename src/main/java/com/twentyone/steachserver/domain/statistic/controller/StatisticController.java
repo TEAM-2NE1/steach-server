c1e9dc986dd027e059d507441559ed590da4b18a
sihyunjojo <si4018@naver.com>
2024-07-21 17:20:33 +0900

    feat: 통계값 반환해주는 메서드 추가

--- src/main/java/com/twentyone/steachserver/domain/statistic/controller/StatisticController.java
+++ src/main/java/com/twentyone/steachserver/domain/statistic/controller/StatisticController.java
@@ -1,11 +1,12 @@
 package com.twentyone.steachserver.domain.statistic.controller;
 
 import com.twentyone.steachserver.domain.member.model.Student;
+import com.twentyone.steachserver.domain.statistic.dto.LectureStatisticsByAllStudentDto;
 import com.twentyone.steachserver.domain.statistic.dto.StatisticsDto;
+import com.twentyone.steachserver.domain.statistic.dto.GPTDataRequestDto;
 import com.twentyone.steachserver.domain.statistic.service.StatisticService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,16 +16,30 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/statistics")
 @RequiredArgsConstructor
 public class StatisticController {
-
     private StatisticService statisticService;
 
     @Operation(summary = "통계 정보 반환 ", description = "무조건 200을 반환")
     @GetMapping
     public ResponseEntity<?> getStatistic(@AuthenticationPrincipal Student student) {
         StatisticsDto statistics = statisticService.getStatistics(student.getId());
-
         return ResponseEntity
                 .status(HttpStatus.CREATED)
+                .body(statistics);
+    }
+
+    @Operation(summary = "GPT 문장 반환 ", description = "무조건 200을 반환")
+    @GetMapping("/gpt")
+    public ResponseEntity<?> getGPTStatistic(@AuthenticationPrincipal Student student, @RequestBody GPTDataRequestDto gptDataRequestDto) {
+        String gptString = statisticService.createGPTString(student, gptDataRequestDto);
+        return ResponseEntity.ok()
+                .body(gptString);
+    }
+
+    @Operation(summary = "강의에 대한 전체 학생에 대한 통계 반환 ", description = "무조건 200을 반환")
+    @GetMapping("/lecture/{lectureId}")
+    public ResponseEntity<?> getLectureStatisticsByAllStudent(@PathVariable Integer lectureId) {
+        LectureStatisticsByAllStudentDto statistics = statisticService.getLectureStatisticsByAllStudent(lectureId);
+        return ResponseEntity.ok()
                 .body(statistics);
     }
 }
---
+++ src/main/java/com/twentyone/steachserver/domain/statistic/model/LectureStatisticsByAllStudent.java
@@ -0,0 +1,37 @@
+package com.twentyone.steachserver.domain.statistic.model;
+
+import com.twentyone.steachserver.domain.lecture.model.Lecture;
+import com.twentyone.steachserver.domain.member.model.Student;
+import com.twentyone.steachserver.domain.studentLecture.dto.LectureStatisticsByAllStudentDto;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Document(collection = "lecture_statistics_by_all_student")
+@Getter
+public class LectureStatisticsByAllStudent {
+    @Id
+    private ObjectId id;
+    private final Integer lectureId;
+    private final Integer averageQuizTotalScore;
+    private final Integer averageQuizAnswerCount;
+    private final Integer averageFocusTime;
+    private final BigDecimal averageFocusRatio;
+
+    private LectureStatisticsByAllStudent(Lecture lecture, Integer averageQuizTotalScore, Integer averageQuizAnswerCount, Integer averageFocusTime, BigDecimal averageFocusRatio) {
+        this.lectureId = lecture.getId();
+        this.averageQuizTotalScore = averageQuizTotalScore;
+        this.averageQuizAnswerCount = averageQuizAnswerCount;
+        this.averageFocusTime = averageFocusTime;
+        this.averageFocusRatio = averageFocusRatio;
+    }
+
+    public static LectureStatisticsByAllStudent of(Lecture lecture, Integer averageQuizTotalScore, Integer averageQuizAnswerCount, Integer averageFocusTime, BigDecimal averageFocusRatio) {
+        return new LectureStatisticsByAllStudent(lecture, averageQuizTotalScore, averageQuizAnswerCount, averageFocusTime, averageFocusRatio);
+    }
+
+
+}
