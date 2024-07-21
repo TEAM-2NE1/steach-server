1902c190a8aca9fe736ce0489b2011ff5ad433ec
sihyunjojo <si4018@naver.com>
2024-07-21 15:19:32 +0900

    refactor: @AuthenticationPrincipal 적용

--- src/main/java/com/twentyone/steachserver/domain/statistic/controller/StatisticController.java
+++ src/main/java/com/twentyone/steachserver/domain/statistic/controller/StatisticController.java
@@ -1,5 +1,6 @@
 package com.twentyone.steachserver.domain.statistic.controller;
 
+import com.twentyone.steachserver.domain.member.model.Student;
 import com.twentyone.steachserver.domain.statistic.dto.StatisticsDto;
 import com.twentyone.steachserver.domain.statistic.service.StatisticService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -7,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,8 +20,8 @@ public class StatisticController {
 
     @Operation(summary = "통계 정보 반환 ", description = "무조건 200을 반환")
     @GetMapping
-    public ResponseEntity<?> getStatistic(String studentUsername) {
-        StatisticsDto statistics = statisticService.getStatistics(studentUsername);
+    public ResponseEntity<?> getStatistic(@AuthenticationPrincipal Student student) {
+        StatisticsDto statistics = statisticService.getStatistics(student.getId());
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
