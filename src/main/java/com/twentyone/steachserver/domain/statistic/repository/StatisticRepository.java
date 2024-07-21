11e3e24618c2046dc2bbce599cb1db4e980757c2
sihyunjojo <si4018@naver.com>
2024-07-21 17:20:18 +0900

    comment: 주석 삭제

--- src/main/java/com/twentyone/steachserver/domain/statistic/repository/StatisticRepository.java
+++ src/main/java/com/twentyone/steachserver/domain/statistic/repository/StatisticRepository.java
@@ -3,5 +3,7 @@ package com.twentyone.steachserver.domain.statistic.repository;
 import com.twentyone.steachserver.domain.statistic.model.Statistic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StatisticRepository extends JpaRepository<Statistic, Integer> {
 }
