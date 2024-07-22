f4baa529632e62450e04a2c2cc6b2734a716c5b6
sihyunjojo <si4018@naver.com>
2024-07-23 00:19:10 +0900

    feat: strip()을 이용한 공백 제거 추가

--- src/main/java/com/twentyone/steachserver/domain/quiz/dto/QuizResponseDto.java
+++ src/main/java/com/twentyone/steachserver/domain/quiz/dto/QuizResponseDto.java
@@ -1,9 +1,6 @@
 package com.twentyone.steachserver.domain.quiz.dto;
 
 import com.twentyone.steachserver.domain.quiz.model.Quiz;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
@@ -15,6 +12,8 @@ public record QuizResponseDto(
         List<String> answers
 ) {
     public static QuizResponseDto createQuizResponseDto(Integer lectureId, QuizRequestDto request) {
+        allStrip(request.choices());
+        allStrip(request.answers());
         return new QuizResponseDto(
                 lectureId,
                 request.quizNumber(),
@@ -25,6 +24,8 @@ public record QuizResponseDto(
     }
 
     public static QuizResponseDto createQuizResponseDto(Quiz quiz, List<String> choices, List<String> answers) {
+        allStrip(choices);
+        allStrip(answers);
         return new QuizResponseDto(
                 quiz.getLecture().getId(),
                 quiz.getQuizNumber(),
@@ -32,5 +33,9 @@ public record QuizResponseDto(
                 choices,
                 answers
         );
+    }
+
+    private static void allStrip(List<String> strings){
+        strings.replaceAll(String::strip);
     }
 }
