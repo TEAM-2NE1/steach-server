9a3cfc67a2967ec122473dc23afe95c43e223f18
lime <joohr1234@gmail.com>
2024-07-22 14:23:24 +0900

    feat: security 권한관련 어노테이션 설정 추가
    @Secured(ROLE_TEACHER) 어노테이션을 사용할 수 있도록 권한 추가

--- src/main/java/com/twentyone/steachserver/domain/member/memberController/TeacherController.java
+++ src/main/java/com/twentyone/steachserver/domain/member/memberController/TeacherController.java
@@ -8,9 +8,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Secured("ROLE_TEACHER")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
