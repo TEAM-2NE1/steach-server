65434354b0ff2f82d6b5a8047688274b07fe8f0a
sihyunjojo <si4018@naver.com>
2024-07-23 19:45:03 +0900

    fix: 잘못된 로직 변경

--- src/main/java/com/twentyone/steachserver/domain/statistic/service/StatisticServiceImpl.java
+++ src/main/java/com/twentyone/steachserver/domain/statistic/service/StatisticServiceImpl.java
@@ -1,6 +1,7 @@
 package com.twentyone.steachserver.domain.statistic.service;
 
 import com.twentyone.steachserver.domain.curriculum.model.Curriculum;
+import com.twentyone.steachserver.domain.curriculum.repository.CurriculumDetailRepository;
 import com.twentyone.steachserver.domain.curriculum.repository.CurriculumRepository;
 import com.twentyone.steachserver.domain.lecture.model.Lecture;
 import com.twentyone.steachserver.domain.member.model.Student;
@@ -8,7 +9,6 @@ import com.twentyone.steachserver.domain.statistic.dto.radarChartStatisticDto;
 import com.twentyone.steachserver.domain.statistic.dto.GPTDataRequestDto;
 import com.twentyone.steachserver.domain.statistic.model.mongo.GPTDataByLecture;
 import com.twentyone.steachserver.domain.statistic.model.mongo.LectureStatisticsByAllStudent;
-import com.twentyone.steachserver.domain.statistic.dto.temp.LectureStatisticsByStudentDto;
 import com.twentyone.steachserver.domain.statistic.repository.GPTDataByLectureMongoRepository;
 import com.twentyone.steachserver.domain.statistic.repository.LectureStatisticMongoRepository;
 import com.twentyone.steachserver.domain.statistic.repository.RadarChartStatisticRepository;
@@ -18,8 +18,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.*;
 
 @Service
@@ -32,6 +30,7 @@ public class StatisticServiceImpl implements StatisticService {
     private final RadarChartStatisticRepository radarChartStatisticRepository;
     private final LectureStatisticMongoRepository lectureStatisticMongoRepository;
     private final GPTDataByLectureMongoRepository gptDataByLectureMongoRepository;
+    private final CurriculumDetailRepository curriculumDetailRepository;
 
     final int NUMBER_OF_CATEGORIES = 7;
 
@@ -163,19 +162,8 @@ public class StatisticServiceImpl implements StatisticService {
         Curriculum curriculum = curriculumRepository.findByLecturesContaining(lecture)
                 .orElseThrow(() -> new IllegalStateException("curriculum not found"));
 
-        // 학생별로 데이터를 모으기 위한 맵
-        Map<Integer, LectureStatisticsByStudentDto> studentStatisticsMap = new HashMap<>();
-
         for (StudentLecture studentLecture : allStudentInfoByLectureId) {
-            Student student = studentLecture.getStudent();
-
-            studentStatisticsMap
-                    .computeIfAbsent(student.getId(), k -> LectureStatisticsByStudentDto.of(student, lecture))
-                    .addLectureData(studentLecture);
-        }
-
-        for (LectureStatisticsByStudentDto lectureStatisticsByStudent : studentStatisticsMap.values()) {
-            GPTDataByLecture gptDataByLecture = GPTDataByLecture.of(lecture, curriculum, lectureStatisticsByStudent);
+            GPTDataByLecture gptDataByLecture = GPTDataByLecture.of(lecture, curriculum, studentLecture);
             gptDataByLectureMongoRepository.save(gptDataByLecture);
         }
     }
