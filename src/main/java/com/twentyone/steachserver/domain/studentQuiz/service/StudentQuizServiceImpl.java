a4c2ca0acaf2a5ae513f7ae9240938661336578b
조시현 <si4018@naver.com>
2024-07-23 12:12:14 +0900

    feat: 검증 로직 추가
    끝난 강의에 대해서 값 변경 못하게 변경 및
    강의에 대한 권한이 있는 사람에 대한 검증

--- src/main/java/com/twentyone/steachserver/domain/studentQuiz/service/StudentQuizServiceImpl.java
+++ src/main/java/com/twentyone/steachserver/domain/studentQuiz/service/StudentQuizServiceImpl.java
@@ -1,9 +1,12 @@
 package com.twentyone.steachserver.domain.studentQuiz.service;
 
+import com.twentyone.steachserver.domain.lecture.model.Lecture;
+import com.twentyone.steachserver.domain.lecture.validator.LectureValidator;
 import com.twentyone.steachserver.domain.member.model.Student;
 import com.twentyone.steachserver.domain.member.repository.StudentRepository;
 import com.twentyone.steachserver.domain.quiz.model.Quiz;
 import com.twentyone.steachserver.domain.quiz.repository.QuizRepository;
+import com.twentyone.steachserver.domain.studentLecture.model.StudentLecture;
 import com.twentyone.steachserver.domain.studentQuiz.dto.StudentQuizRequestDto;
 import com.twentyone.steachserver.domain.studentQuiz.model.StudentQuiz;
 import com.twentyone.steachserver.domain.studentQuiz.model.StudentQuizId;
@@ -12,6 +15,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,10 +26,17 @@ public class StudentQuizServiceImpl implements StudentQuizService {
     private final StudentRepository studentRepository;
     private final QuizRepository quizRepository;
 
+    private final LectureValidator lectureValidator;
+
     @Transactional
     public StudentQuiz createStudentQuiz(Integer studentId, Integer quizId, StudentQuizRequestDto requestDto){
-        Student student = studentRepository.getReferenceById(studentId);
-        Quiz quiz = quizRepository.getReferenceById(quizId);
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 학생입니다."));
+        Quiz quiz = quizRepository.findById(quizId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 퀴즈입니다."));
+
+        lectureValidator.validateFinishLecture(quiz.getLecture());
+        lectureValidator.validateQuizOfLectureAuth(quiz, student);
 
         Optional<StudentQuiz> studentQuiz = studentQuizzesRepository.findById(StudentQuizId.createStudentQuizId(studentId, quizId));
         if (studentQuiz.isPresent()) throw new IllegalArgumentException("이미 점수가 존재합니다.");
