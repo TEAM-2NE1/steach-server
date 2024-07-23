a988eccfc6e8dbe98ec56762e754b0eb7138aaa4
조시현 <si4018@naver.com>
2024-07-23 12:43:15 +0900

    feat: MaxAttendees의 입력 대한 검증로직 추가

---
+++ src/main/java/com/twentyone/steachserver/domain/curriculum/validator/CurriculumValidator.java
@@ -0,0 +1,12 @@
+package com.twentyone.steachserver.domain.curriculum.validator;
+
+import com.twentyone.steachserver.domain.curriculum.dto.CurriculumAddRequest;
+
+public class CurriculumValidator {
+
+    public void validatorMaxAttendees(CurriculumAddRequest request) {
+        if (1 <= request.getMaxAttendees() && request.getMaxAttendees() <= 4 ) {
+            throw new IllegalArgumentException("최대 정원은 1이상 4이하 여야합니다.");
+        }
+    }
+}
