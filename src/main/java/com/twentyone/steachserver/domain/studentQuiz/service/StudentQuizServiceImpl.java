987f0ce859df53843e7d3983b169f8bac250fc5c
조시현 <si4018@naver.com>
2024-07-23 10:11:49 +0900

    feat: 이미 존재하는 점수가 있을시 예외 터뜨려줌

--- src/main/java/com/twentyone/steachserver/domain/studentQuiz/service/StudentQuizServiceImpl.java
+++ src/main/java/com/twentyone/steachserver/domain/studentQuiz/service/StudentQuizServiceImpl.java
@@ -6,10 +6,13 @@ import com.twentyone.steachserver.domain.quiz.model.Quiz;
 import com.twentyone.steachserver.domain.quiz.repository.QuizRepository;
 import com.twentyone.steachserver.domain.studentQuiz.dto.StudentQuizRequestDto;
 import com.twentyone.steachserver.domain.studentQuiz.model.StudentQuiz;
+import com.twentyone.steachserver.domain.studentQuiz.model.StudentQuizId;
 import com.twentyone.steachserver.domain.studentQuiz.repository.StudentQuizRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +26,9 @@ public class StudentQuizServiceImpl implements StudentQuizService {
     public StudentQuiz createStudentQuiz(Integer studentId, Integer quizId, StudentQuizRequestDto requestDto){
         Student student = studentRepository.getReferenceById(studentId);
         Quiz quiz = quizRepository.getReferenceById(quizId);
+
+        Optional<StudentQuiz> studentQuiz = studentQuizzesRepository.findById(StudentQuizId.createStudentQuizId(studentId, quizId));
+        if (studentQuiz.isPresent()) throw new IllegalArgumentException("이미 점수가 존재합니다.");
 
         StudentQuiz newStudentQuiz = StudentQuiz.createStudentQuiz(student, quiz, requestDto);
         studentQuizzesRepository.save(newStudentQuiz);
