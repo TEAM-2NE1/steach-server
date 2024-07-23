6bb344c883de9242f71e6542546a8a93a4decdd6
sihyunjojo <si4018@naver.com>
2024-07-24 01:43:29 +0900

    feat: etc를 제외한 사이즈 반환하는 메서드 추가

--- src/main/java/com/twentyone/steachserver/domain/curriculum/enums/CurriculumCategory.java
+++ src/main/java/com/twentyone/steachserver/domain/curriculum/enums/CurriculumCategory.java
@@ -3,15 +3,21 @@ package com.twentyone.steachserver.domain.curriculum.enums;
 import java.util.List;
 
 public enum CurriculumCategory {
+    // ETC는 항상 뒤에 해주세요!
     KOREAN, MATH, FOREIGN_LANGUAGE, SCIENCE, ENGINEERING, ARTS_AND_PHYSICAL, EDUCATION, ETC;
 
     public static int size() {
         return CurriculumCategory.values().length;
     }
 
+    public static int sizeExcludingETC() {
+        return CurriculumCategory.values().length - 1;
+    }
+
     public static List<CurriculumCategory> getCategories() {
         return List.of(CurriculumCategory.values());
     }
+
     public static CurriculumCategory getCategoryByIndex(int index) {
         CurriculumCategory[] categories = CurriculumCategory.values();
         if (index < 0 || index >= categories.length) {
