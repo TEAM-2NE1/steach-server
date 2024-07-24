56453e9834ce575aee859bd662d705ed728008dc
lime <joohr1234@gmail.com>
2024-07-24 14:15:17 +0900

    fix: LectureRepository JPA named parameter 에러 해결
    @Param 누락 추가

--- src/main/java/com/twentyone/steachserver/domain/lecture/repository/LectureRepository.java
+++ src/main/java/com/twentyone/steachserver/domain/lecture/repository/LectureRepository.java
@@ -7,10 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LectureRepository extends JpaRepository<Lecture, Integer> {
     List<Lecture> findByLectureStartDateBetween(LocalDateTime toMinute, LocalDateTime fromMinute);
 
     @Query(value = "select * from lectures where curriculum_id = :curriculumId", nativeQuery = true)
-    Optional<List<Lecture>> findByCurriculumId(Integer curriculumId);
+    Optional<List<Lecture>> findByCurriculumId(@Param("curriculumId") Integer curriculumId);
 }
