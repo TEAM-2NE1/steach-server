52b6a165db2728937454313bb5007b70929e2731
lime <joohr1234@gmail.com>
2024-07-24 14:45:59 +0900

    fix: CurriculumDetailResponse에 createdAt 추가

--- src/main/java/com/twentyone/steachserver/domain/curriculum/dto/CurriculumListResponse.java
+++ src/main/java/com/twentyone/steachserver/domain/curriculum/dto/CurriculumListResponse.java
@@ -1,9 +1,7 @@
 package com.twentyone.steachserver.domain.curriculum.dto;
 
 import com.twentyone.steachserver.domain.curriculum.model.Curriculum;
-import com.twentyone.steachserver.domain.curriculum.model.CurriculumDetail;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,13 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CurriculumListResponse {
-    // @Builder를 사용할 때 초기화 표현식이 무시된다
-    // 아래 표현식을 통해서 빌더에서 초기화를 할 수 있다.
-    @Builder.Default
     private List<CurriculumDetailResponse> curricula = new ArrayList<>();
 
     public static CurriculumListResponse fromDomainList(List<Curriculum> curriculaList) {
