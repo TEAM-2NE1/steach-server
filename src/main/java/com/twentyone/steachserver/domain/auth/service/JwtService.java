3d3e676b40ca895c4fce6e11729f96301a15c6c3
lime <joohr1234@gmail.com>
2024-07-29 13:16:25 +0900

    feat: 토큰 만료 커스텀 에러메시지 추가

--- src/main/java/com/twentyone/steachserver/domain/auth/service/JwtService.java
+++ src/main/java/com/twentyone/steachserver/domain/auth/service/JwtService.java
@@ -1,8 +1,6 @@
 package com.twentyone.steachserver.domain.auth.service;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
@@ -25,7 +23,7 @@ public class JwtService {
     @Value("${jwt.secretKey}")
     private String SECRET_KEY;
 
-    public static long accessTokenValidTime = Duration.ofMinutes(200).toMillis(); // 만료시간 30분
+    public static long accessTokenValidTime = Duration.ofMinutes(1).toMillis(); // 만료시간 30분
     public static long passwordAuthTokenValidTime = Duration.ofMinutes(1).toMillis(); // 만료시간 1분
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
@@ -70,6 +68,16 @@ public class JwtService {
         return b;
     }
 
+    public void validateToken(String token, UserDetails userDetails) {
+//        if (isTokenExpired(token)) {
+//            throw new ExpiredJwtException();
+//        }
+        final String username = extractUsername(token);
+        if (!username.equals(userDetails.getUsername())) {
+            throw new JwtException("사용자 에러");
+        }
+    }
+
     public boolean isPasswordAuthTokenValid(String token, UserDetails userDetails) {
         try {
             final String username = extractUsername(token);
@@ -100,7 +108,6 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
-
     }
 
     private SecretKey getSignInKey() {
