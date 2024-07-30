85f12eb7f351787070ba2e611761bcc2521c1f89
조시현 <si4018@naver.com>
2024-07-30 13:24:20 +0900

    refactor: 메서드 uri 변경

--- src/main/java/com/twentyone/steachserver/config/HealthController.java
+++ src/main/java/com/twentyone/steachserver/config/HealthController.java
@@ -2,12 +2,14 @@ package com.twentyone.steachserver.config;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class HealthController {
-    @GetMapping("/actuator/health")
-    public ResponseEntity<String> health() {
+    @GetMapping("/check/server/operating")
+    public ResponseEntity<String> CheckServerOperating() {
         return ResponseEntity.ok("OK");
     }
 }
