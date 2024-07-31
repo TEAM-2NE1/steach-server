38db3b63f45e7ad975515a68dd1dacc323b5c744
sihyunjojo <si4018@naver.com>
2024-07-31 22:44:16 +0900

    refactor: redisTemplate Generic 으로 관리

--- src/main/java/com/twentyone/steachserver/config/RedisConfig.java
+++ src/main/java/com/twentyone/steachserver/config/RedisConfig.java
@@ -1,5 +1,4 @@
 package com.twentyone.steachserver.config;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -26,44 +25,47 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int port;
 
+    // RedisConnectionFactory를 빈으로 정의하는 메서드입니다.
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+    public RedisConnectionFactory redisConnectionFactory() {
+        // LettuceConnectionFactory를 사용하여 Redis 연결 팩토리를 생성합니다.
+        return new LettuceConnectionFactory(host, port);
+    }
 
-        // 일반적인 key:value의 경우 시리얼라이저
+    @Bean
+    public <T> RedisTemplate<String, T> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, T> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        // 키를 문자열로 직렬화
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        // 값을 JSON 형식으로 직렬화
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
-        // Hash를 사용할 경우 시리얼라이저
+        // Hash를 사용할 경우 시리얼라이저를 설정합니다. (문자열로 직렬화)
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-
-        // 모든 경우
-        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         return redisTemplate;
     }
 
-
+    // CacheManager를 빈으로 정의하는 메서드입니다.
     @Bean
     public CacheManager rcm(RedisConnectionFactory cf) {
+        // Redis 캐시 구성 설정을 정의합니다.
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                // 키를 직렬화하는 방법을 설정합니다. (문자열로 직렬화)
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                // 값을 직렬화하는 방법을 설정합니다. (제네릭 Jackson JSON 직렬화)
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                // 캐시 항목의 기본 TTL(수명)을 3분으로 설정합니다.
                 .entryTtl(Duration.ofMinutes(3L));
 
+        // RedisCacheManager를 구성하여 반환합니다.
         return RedisCacheManager
                 .RedisCacheManagerBuilder
                 .fromConnectionFactory(cf)
                 .cacheDefaults(redisCacheConfiguration)
                 .build();
     }
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
-
-
 }
