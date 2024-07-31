4fbf6806d17b601f00ae2ae6d64cc7c7593001ef
sihyunjojo <si4018@naver.com>
2024-08-01 01:29:28 +0900

    refactor: 예외 던지는 부분 삭제

--- src/main/java/com/twentyone/steachserver/domain/quiz/service/QuizChoiceService.java
+++ src/main/java/com/twentyone/steachserver/domain/quiz/service/QuizChoiceService.java
@@ -5,7 +5,7 @@ import com.twentyone.steachserver.domain.quiz.model.Quiz;
 import java.util.List;
 
 public interface QuizChoiceService {
-    void createQuizChoices(List<String> choices, List<String> answers, Quiz savedQuiz) throws RuntimeException;
+    void createQuizChoices(List<String> choices, List<String> answers, Quiz savedQuiz);
     List<String> getAnswers(Quiz quiz);
     List<String> getChoices(Quiz quiz);
 }
