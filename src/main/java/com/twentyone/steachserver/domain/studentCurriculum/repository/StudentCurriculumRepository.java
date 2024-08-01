05d62da8332fdc69927c08a7cb559ae4f321f07e
조시현 <si4018@naver.com>
2024-08-01 13:40:35 +0900

    feat: 강의 신청 취소 메서드 추가

--- src/main/java/com/twentyone/steachserver/domain/studentCurriculum/repository/StudentCurriculumRepository.java
+++ src/main/java/com/twentyone/steachserver/domain/studentCurriculum/repository/StudentCurriculumRepository.java
@@ -4,6 +4,7 @@ import com.twentyone.steachserver.domain.curriculum.model.Curriculum;
 import com.twentyone.steachserver.domain.member.model.Student;
 import com.twentyone.steachserver.domain.studentCurriculum.model.StudentCurriculum;
 import com.twentyone.steachserver.domain.studentCurriculum.model.StudentCurriculumId;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -24,5 +25,13 @@ public interface StudentCurriculumRepository extends JpaRepository<StudentCurric
 
     List<StudentCurriculum> findAllByCurriculumId(Integer curriculumId);
 
-    void deleteByStudentAndCurriculum(Student student, Curriculum referenceById);
+    @Transactional
+    default void deleteByStudentAndCurriculumWithException(Student student, Curriculum curriculum) {
+        long deletedCount = this.deleteByStudentAndCurriculum(student, curriculum);
+        if (deletedCount == 0) {
+            throw new IllegalArgumentException("해당 커리큘럼에 대해서 수강 신청 내역이 존재하지 않았습니다.");
+        }
+    }
+
+    long deleteByStudentAndCurriculum(Student student, Curriculum referenceById);
 }
