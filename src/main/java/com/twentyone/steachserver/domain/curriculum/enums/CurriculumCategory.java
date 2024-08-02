6ba692e149858669c1c5d6c8879940ac25ad4215
lime <joohr1234@gmail.com>
2024-08-02 12:52:31 +0900

    fix: 커리큘럼 카테고리 종류 변경 EDUCATION -> SOCIAL
    - 배포 전 꼭 디비 바꾸기!!!

--- src/main/java/com/twentyone/steachserver/domain/curriculum/enums/CurriculumCategory.java
+++ src/main/java/com/twentyone/steachserver/domain/curriculum/enums/CurriculumCategory.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public enum CurriculumCategory {
     // ETC는 항상 뒤에 해주세요!
-    KOREAN, MATH, FOREIGN_LANGUAGE, SCIENCE, ENGINEERING, ARTS_AND_PHYSICAL, EDUCATION, ETC;
+    KOREAN, MATH, FOREIGN_LANGUAGE, SCIENCE, ENGINEERING, ARTS_AND_PHYSICAL, SOCIAL, ETC;
 
     public static int size() {
         return CurriculumCategory.values().length;
