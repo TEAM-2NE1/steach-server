72779251bb448f0e7321357b126a10c585ea3eaf
sihyunjojo <si4018@naver.com>
2024-08-06 22:14:56 +0900

    refactor: 18080과 18008에 대해서 풀어주기

--- src/main/java/com/twentyone/steachserver/config/WebConfig.java
+++ src/main/java/com/twentyone/steachserver/config/WebConfig.java
@@ -46,6 +46,10 @@ public class WebConfig {
         configuration.addAllowedOrigin("http://localhost:8080");
         configuration.addAllowedOrigin("http://localhost:18080");
         configuration.addAllowedOrigin("http://localhost:18008");
+        configuration.addAllowedOrigin("http://steach.ssafy.io:18080");
+        configuration.addAllowedOrigin("http://steach.ssafy.io:18080");
+        configuration.addAllowedOrigin("https://steach.ssafy.io:18008");
+        configuration.addAllowedOrigin("https://steach.ssafy.io:18008");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
