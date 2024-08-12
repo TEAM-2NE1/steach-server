4c9a65be384b6e84d5cc4e9126ede788723413c4
조시현 <si4018@naver.com>
2024-08-12 15:16:05 +0900

    feat: 통계에 어떤 과목인지에 대해서 추가

--- src/main/java/com/twentyone/steachserver/domain/curriculum/enums/CurriculumCategory.java
+++ src/main/java/com/twentyone/steachserver/domain/curriculum/enums/CurriculumCategory.java
@@ -1,10 +1,28 @@
 package com.twentyone.steachserver.domain.curriculum.enums;
 
+import java.util.Arrays;
 import java.util.List;
 
 public enum CurriculumCategory {
     // ETC는 항상 뒤에 해주세요!
-    KOREAN, MATH, FOREIGN_LANGUAGE, SCIENCE, ENGINEERING, ARTS_AND_PHYSICAL, SOCIAL, ETC;
+    KOREAN("국어"), // 한국어 관련 과목
+    MATH("수학"), // 수학 관련 과목
+    FOREIGN_LANGUAGE("외국어"), // 외국어 관련 과목
+    SCIENCE("과학"), // 과학 관련 과목
+    ENGINEERING("공학"), // 공학 관련 과목
+    ARTS_AND_PHYSICAL("예체능"), // 예술 및 체육 관련 과목
+    SOCIAL("사회"), // 사회과학 관련 과목
+    ETC("기타"); // 기타 과목
+
+    private final String description;
+
+    CurriculumCategory(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 
     public static int size() {
         return CurriculumCategory.values().length;
@@ -17,6 +35,13 @@ public enum CurriculumCategory {
     public static List<CurriculumCategory> getCategories() {
         return List.of(CurriculumCategory.values());
     }
+
+    public static List<String> getCategoriesDescription() {
+        return Arrays.stream(CurriculumCategory.values())
+                .map(CurriculumCategory::getDescription)
+                .toList();
+    }
+
 
     public static CurriculumCategory getCategoryByIndex(int index) {
         CurriculumCategory[] categories = CurriculumCategory.values();
