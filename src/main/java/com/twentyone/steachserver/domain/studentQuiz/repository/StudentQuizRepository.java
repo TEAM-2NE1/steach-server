ece5e25c32e4cf8260fa6a4de85c2e36f7e5a521
조시현 <si4018@naver.com>
2024-08-14 10:04:46 +0900

    fix: jpa Repository 에러 해결

--- src/main/java/com/twentyone/steachserver/domain/studentQuiz/repository/StudentQuizRepository.java
+++ src/main/java/com/twentyone/steachserver/domain/studentQuiz/repository/StudentQuizRepository.java
@@ -7,9 +7,10 @@ import com.twentyone.steachserver.domain.studentQuiz.model.StudentQuizId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudentQuizRepository extends JpaRepository<StudentQuiz, StudentQuizId> {
     List<StudentQuiz> findTop4StudentQuizByQuizOrderByScoreDesc(Quiz quiz);
 
-    StudentQuiz findByStudentAndQuiz(Student student, Quiz quiz);
+    Optional<StudentQuiz> findByStudentAndQuiz(Student student, Quiz quiz);
 }
